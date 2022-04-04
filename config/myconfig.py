98d63aa45e78c02bb02320af409e0501410137b7
Kristjan Roosild <kristjan.roosild@wise.com>
2022-04-04 15:19:23 +0300

    add turn boost part to avoid slowdowns while turning

--- config/myconfig.py
+++ config/myconfig.py
@@ -3,3 +3,12 @@ TRANSFORMATIONS = ['CROP']
 ROI_CROP_TOP = 60
 ROI_CROP_KEEP_SIZE = False
 SEQUENCE_LENGTH = 3
+
+JOYSTICK_STEERING_SCALE = 0.8
+
+
+TURN_BOOST_TOGGLE_BUTTON = 'R2'
+
+# Keys are turning angles which equal or bigger, when positive or equal or smaller, when negative need a boost.
+# Values are the throttle increase for during that angle turn.
+TURN_BOOST_CONFIG = {0.8: 0.03}   # - when angle is >= 0.8, then add 0.03 to the throttle
