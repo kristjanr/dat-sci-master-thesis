c3ba7940929e9cbd4ad76a61d5a3b366d6525820
Kristjan Roosild <kristjan.roosild@wise.com>
2022-07-16 20:24:39 +0300

    refactor

--- src/dataloader.py
+++ src/dataloader.py
@@ -95,9 +95,8 @@ def get_folds(data, n_folds=5, chunk_size=100):
     return train_folds, test_folds
 
 
-def get_fold_indices(data_length, n_folds=5, chunk_size=107):
-
-    chunks = split_to_chunks(list(range(data_length)), chunk_size)
+def get_fold_indices(data, n_folds=5, chunk_size=107):
+    chunks = split_to_chunks(list(range(len(data))), chunk_size)
     from sklearn.model_selection import KFold
     kf = KFold(n_splits=n_folds, shuffle=False)
 
